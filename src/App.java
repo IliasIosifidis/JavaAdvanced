1adfc3359b767f03667bfbfdd856fcd47a0ebee1
ilias iosifidis <iosifidis90@gmail.com>
2026-04-18 22:45:18 +0300

    finish project with reflection

--- src/App.java
+++ src/App.java
@@ -1,7 +1,8 @@
 void main() {
   var store = new DataStore<>();
-  store.add(67);
-  System.out.println(
-    store.getById(0)
-  );
+  Class<?> store1 = store.getClass();
+  var user = new User(1,"ilias");
+  User.print(user);
+
+  System.out.println();
 }
--- src/User.java
+++ src/User.java
@@ -1,13 +1,32 @@
-public class User extends BaseEntity{
+import java.lang.reflect.Field;
+
+public class User extends BaseEntity {
 
   private String name;
+  private String lastName;
+  private String veryLastName;
+
+  public static void print(Object obj) {
+    Class<?> clazz = obj.getClass();
+    System.out.println(clazz.getSimpleName() + " {");
+
+    while (clazz != null) {
+      for (Field field : clazz.getDeclaredFields()) {
+        field.setAccessible(true);
+        try {
+          System.out.println("  " + field.getName() + " = " + field.get(obj));
+        } catch (IllegalAccessException e) {
+          System.out.println("  " + field.getName() + " = [inaccessible]");
+        }
+      }
+      clazz = clazz.getSuperclass();
+    }
+
+    System.out.println("}");
+  }
 
   public User(int id, String name) {
     super(id);
     this.name = name;
-  }
-
-  public String getName(){
-    return name;
   }
 }
